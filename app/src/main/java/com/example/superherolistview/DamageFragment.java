57bb8514235e4ce2255b1d3f5a49367d9b3e867a
Edrick <edricktongmario@gmail.com>
2019-12-17 19:55:58 -0800

    12/17/19 Push 2 Need Shorr Help

--- app/src/main/java/com/example/superherolistview/DamageFragment.java
+++ app/src/main/java/com/example/superherolistview/DamageFragment.java
@@ -1,14 +1,31 @@
 package com.example.superherolistview;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ImageView;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
-class DamageFragment extends androidx.fragment.app.Fragment {
+import com.google.gson.Gson;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+
+
+class DamageFragment extends androidx.fragment.app.Fragment {
+    private ListView dammageListView;
+    private TextView damageHeroName;
+    private List<Hero> damageHeroList;
 
 
 
